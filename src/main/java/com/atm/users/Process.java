4b740c18886ffa18b37a0576daf5aa2734e9ceaa
Yusuf Burak Yetiş <76731442+ybyetis@users.noreply.github.com>
2021-06-21 18:01:27 +0300

    Update Process.java

--- src/main/java/com/atm/users/Process.java
+++ src/main/java/com/atm/users/Process.java
@@ -3,15 +3,15 @@ package com.atm.users;
 import com.atm.AtmClass;
 
 public class Process {
-    public void paraYatir(int balance) {
-        balance = balance + 50;
-        System.out.println("Hesabınıza 50 tl yatırıldı ve güncel bakiye: " + balance + " tl");
+    public void paraYatir(int balance, int money) {
+        balance = balance + money;
+        System.out.println("Hesabınıza " + money + " tl yatırıldı ve güncel bakiye: " + balance + " tl");
 
     }
 
-    public void paraCek(int balance) {
-        balance = balance - 50;
-        System.out.println("Hesabınızdan 50 tl çekildi ve güncel bakiye: " + balance + " tl");
+    public void paraCek(int balance, int money) {
+        balance = balance - money;
+        System.out.println("Hesabınızdan " + money + " tl çekildi ve güncel bakiye: " + balance + " tl");
 
     }
 
